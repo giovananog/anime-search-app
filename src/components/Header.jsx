ace8f1b9209371759a62c1bbc98fcb5aef8b6f3d
Giovana Nogueira <ongiovana2016@gmail.com>
2024-01-16 21:51:41 -0300

    chore: header better styling and search fields done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,146 @@
-import React from "react";
+import {useState} from 'react'
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import { getAllCategories } from '../api-requests';
+import SvgIcon from '@mui/material/SvgIcon';
+import App from "./App"
 
-function Header() {
-    return (
-      <div>
-        <h1>Anime Search</h1>
-      </div>
-    );
+
+function HomeIcon(props) {
+  return (
+    <SvgIcon {...props}>
+      <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+    </SvgIcon>
+  );
 }
 
-export default Header;
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  width: '100%',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
+
+
+export default function Header() {
+  
+  async function searchCategory(event) {
+    var value = event.target.value;
+    if (event.key === "Enter") {
+      setData({
+        'filter': 'categories',
+        'value': value,
+      });
+    }
+  }
+  
+  async function searchName(event) {
+    var value = event.target.value;
+    if (event.key === "Enter") {
+      setData({
+        'filter': 'text',
+        'value': value,
+      });
+    }
+  }
+
+  function handleClick () {
+    window.location.reload();
+  }
+
+  const [data, setData] = useState();
+
+  return (
+    <div>
+
+    {data ? (<App filter={data.filter} info={data.value} i={data.i}/>) : 
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            onClick={handleClick}
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            <HomeIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            ANIME SEARCH
+          </Typography>
+          <Autocomplete
+      disablePortal
+      id="combo-box-demo"
+      options={allCategoriesList.filter((elem) => elem !== undefined)}
+      onKeyDown={searchCategory}
+      sx={{ width: 250 }}
+      renderInput={(params) => <TextField {...params} label="Categories" />}
+    />
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+              onKeyDown={searchName}
+            />
+          </Search>
+        </Toolbar>
+      </AppBar>
+    </Box>
+    }
+    </div>
+  );
+}
+
+const allCategoriesList = await getAllCategories(200);
