d7cfcddb4b1f731fc12fdc9e60c41abd39960bab
Giovana Nogueira <ongiovana2016@gmail.com>
2024-01-16 21:40:12 -0300

    feat: api requests functions added

---
+++ src/api-requests.js
@@ -0,0 +1,62 @@
+import axios from 'axios';
+
+const apiBaseUrl = 'https://kitsu.io/api/edge';
+
+export async function getRandomAnime() {
+  try {
+    const response = await axios.get(`${apiBaseUrl}/anime/${Math.floor(Math.random() * 1598)}`);
+    return response.data.data;
+  } catch (error) {
+    console.error('Failed to make request:', error.message);
+    throw error; 
+  }
+}
+
+export async function getMultipleRandomAnime(count) {
+  const requests = Array.from({ length: count }, () => getRandomAnime());
+  try {
+    const results = await Promise.all(requests);
+    return results;
+  } catch (error) {
+    console.error('Failed to make requests:', error.message);
+    throw error;
+  }
+}
+
+export async function getCategories(count) {
+    try {
+      const response = await axios.get(`https://kitsu.io/api/edge/categories/` + count);
+      
+      return {
+        label: response.data.data.attributes.title
+      }
+    } catch (error) {
+      console.error('Failed to make request:', error.message);
+    }
+  }
+
+
+  export async function getAllCategories(count) {
+    const requests = [];
+
+    for (let i = 1; i <= count; i++) {
+      requests.push(getCategories(i));
+    }
+    try {
+      const results = await Promise.all(requests);
+      return results;
+    } catch (error) {
+      console.error('Failed to make requests:', error.message);
+      throw error;
+    }
+  }
+
+  export async function filterSearch(filter, info) {
+    try {
+      const response = await axios.get(`https://kitsu.io/api/edge/anime?filter[` + filter + ']=' + info);
+      
+      return response.data.data;
+    } catch (error) {
+      console.error('Failed to make request:', error.message);
+    }
+  }
